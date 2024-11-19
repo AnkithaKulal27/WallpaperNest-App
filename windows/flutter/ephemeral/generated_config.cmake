2c9a2ad1c2b3946c25484d647e19c305a06aafe3
Ankitha <ankithakulal57@gmail.com>
2024-11-19 21:02:01 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Ankitha\\MSC project\\2nd sem mini project-WallPaperApp\\wallpaperapp" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=D:\\Ankitha\\MSC project\\2nd sem mini project-WallPaperApp\\wallpaperapp"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Ankitha\\MSC project\\2nd sem mini project-WallPaperApp\\wallpaperapp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Ankitha\\MSC project\\2nd sem mini project-WallPaperApp\\wallpaperapp"
+  "FLUTTER_TARGET=D:\\Ankitha\\MSC project\\2nd sem mini project-WallPaperApp\\wallpaperapp\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Ankitha\\MSC project\\2nd sem mini project-WallPaperApp\\wallpaperapp\\.dart_tool\\package_config.json"
+)
